f90562eb089756b6e4c9995f4e4747fe97cf64db
heziye <heziye@qq.com>
2021-11-10 20:29:54 +0800

    加载css,图片,字体配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css"; //通过访问对象的形式来获取样式
+import Robot from "./components/Robot";
+import robots from "./mockdata/robots.json";
+import logo from "./assets/images/logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<div className={styles.appHeader}>
+				<img src={logo} alt="logo" className={styles.appLogo} />
+				<h1>罗伯特机器人</h1>
+			</div>
+			<div className={styles.robotList}>
+				{robots.map((item) => {
+					return (
+						<Robot
+							// key={item.id}
+							name={item.name}
+							id={item.id}
+							email={item.email}
+						></Robot>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
